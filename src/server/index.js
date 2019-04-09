da2073494a32e5d6d0b2079910ed1c23c9c98160
Carmen Peralta <cperalta26@gmail.com>
2019-04-09 16:29:30 -0400

    move secrets, client, and server to src directory

--- src/server/index.js
+++ src/server/index.js
@@ -55,7 +55,7 @@ const createApp = () => {
   app.use('/api', require('./api'))
 
   // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')))
+  app.use(express.static(path.join(__dirname, '..', '..', 'public')))
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
   .use((req, res, next) => {
@@ -70,7 +70,7 @@ const createApp = () => {
 
   // sends index.html
   app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+    res.sendFile(path.join(__dirname, '..', '..', 'public/index.html'))
   })
 
   // error handling endware
