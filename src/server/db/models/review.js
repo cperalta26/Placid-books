9a1b375a648940a21809f9ec25d254d3240e5941
Carmen Peralta <cperalta26@gmail.com>
2019-04-17 21:04:24 -0400

    update review model

--- src/server/db/models/review.js
+++ src/server/db/models/review.js
@@ -2,7 +2,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Review = db.define('review', {
-
+  rating: {
+    type: Sequelize.DECIMAL
+  },
+  reviewComment: {
+    type: Sequelize.TEXT
+  }
 })
 
-module.export = Review
+module.exports = Review
