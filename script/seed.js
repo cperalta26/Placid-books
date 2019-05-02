e561d1466627c3ee12712d2728089a0005cc84a2
Carmen Peralta <cperalta26@gmail.com>
2019-05-02 17:36:46 -0400

    feat - seed one book

--- script/seed.js
+++ script/seed.js
@@ -1,5 +1,5 @@
 const db = require('../src/server/db')
-const {User} = require('../src/server/db/models')
+const {User, Book} = require('../src/server/db/models')
 
 async function seed () {
   await db.sync({force: true})
@@ -10,7 +10,21 @@ async function seed () {
     User.create({email: 'murphy@email.com', password: '123'})
   ])
 
-  console.log(`seeded ${users.length} users`)
+  console.log(`seeded ${users.length} user${users.length > 1 ? 's' : ''}`)
+
+  const books = await Promise.all([
+    Book.create({
+      title: 'The Wonderful Things You Will Be',
+      description: "From brave and bold to creative and clever, Emily Winfield Martin's rhythmic rhyme expresses all the loving things that parents think of when they look at their children. With beautiful, and sometimes humorous, illustrations, and a clever gatefold with kids in costumes, this is a book grown-ups will love reading over and over to kids—both young and old.",
+      numOfPages: 36,
+      publishedDate: '2015-08-25',
+      coverImg: 'https://images-na.ssl-images-amazon.com/images/I/91HHxxtA1wL.jpg',
+      price: 9.99,
+      quantity: 1000,
+      available: true
+    })
+  ])
+  console.log(`seeded ${books.length} book${books.length > 1 ? 's' : ''}`)
   console.log(`seeded successfully`)
 }
 
