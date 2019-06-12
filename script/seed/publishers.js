3e58d8b6a9098d8df7c52a843a4cf0db3aaff720
Carmen Peralta <cperalta26@gmail.com>
2019-06-12 09:22:40 -0400

    add more publishers to seed file

--- script/seed/publishers.js
+++ script/seed/publishers.js
@@ -28,7 +28,67 @@ const publishers = [
   },
   {
     publisherName: 'North-South Books'
-  }
+  },
+  {
+    publisherName: 'Knopf Books for Young Readers'
+  },
+  {
+    publisherName: 'The Treasured Tree'
+  },
+  {
+    publisherName: 'Harper & Row'
+  },
+  {
+    publisherName: 'Farrar, Straus and Giroux'
+  },
+  {
+    publisherName: 'Grosset & Dunlap'
+  },
+  {
+    publisherName: 'Philomel Books'
+  },
+  {
+    publisherName: 'Child’s Play International'
+  },
+  {
+    publisherName: 'Chronicle Books'
+  },
+  {
+    publisherName: 'Henry Holt and Co'
+  },
+  {
+    publisherName: 'Priddy Books'
+  },
+  {
+    publisherName: 'Hyperion Books for Children'
+  },
+  {
+    publisherName: 'Two Lions'
+  },
+  {
+    publisherName: 'HMH Books for Young Readers'
+  },
+  {
+    publisherName: 'Kids Can Press'
+  },
+  {
+    publisherName: 'Little Simon'
+  },
+  {
+    publisherName: 'Little Brown Books for Young Readers'
+  },
+  {
+    publisherName: 'Sourcebooks Wonderland'
+  },
+  {
+    publisherName: 'Viking Books for Young Readers'
+  },
+  {
+    publisherName: 'Feiwel & Friends'
+  },
+  {
+    publisherName: 'Harry N. Abrams'
+  },
 ]
 
 module.exports = {
