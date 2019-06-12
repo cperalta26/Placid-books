806e9b68ff9c21220e4615d3f8dae31cb089e544
Carmen Peralta <cperalta26@gmail.com>
2019-06-12 14:38:40 -0400

    add one author to seed file

--- script/seed/authors.js
+++ script/seed/authors.js
@@ -1,4 +1,13 @@
 const authors = [
+  {
+    firstName: 'Emily',
+    lastName: 'Winfield Martin',
+    aboutAuthor: `Emily Winfield Martin makes paintings, books and other things. When she was small, she spent every moment drawing, reading, dressing rabbits in fancy clothes, and having many peculiar daydreams. When she grew up, she began to illustrate those daydreams and she created a cottage industry called The Black Apple, which sells all manner of her art and etceteras.
+
+    She works in a tiny nook of a studio filled with old children's books, wind-up toys, and stacks of fabric. She lives in Portland, Or.
+
+    You can visit her at www.emilywinfieldmartin.com`
+  }
 ]
 
 module.exports = {
